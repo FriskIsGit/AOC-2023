fc7425cdba2bde1715b2b2262a5e96e99ab7bb6f
FriskIsGit <friskisgit@users.noreply.github.com>
2023-12-12 00:51:49 +0000

    completed day11

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,7 +1,7 @@
 use std::fmt::{Display, Formatter, Write};
-
+// two solutions can be merged into one with a parameter but that would mean different input parameters
 pub fn galaxy1(lines: Vec<String>) -> usize {
-    let galaxy = parse_input_expanded(lines);
+    let galaxy = parse_input(lines, true);
     let mut galaxies = vec![];
     for (r, row) in galaxy.iter().enumerate() {
         for (c, col) in row.iter().enumerate() {
@@ -35,11 +35,14 @@ fn print_table(arr: &Vec<Vec<u8>>) {
     }
 }
 
-pub fn parse_input_expanded(mut lines: Vec<String>) -> Vec<Vec<u8>> {
+pub fn parse_input(mut lines: Vec<String>, expand: bool) -> Vec<Vec<u8>> {
     let mut galaxy_map = Vec::with_capacity(lines.len());
     for line in lines.iter_mut() {
         let taken_line = std::mem::take(&mut *line);
         galaxy_map.push(taken_line.into_bytes());
+    }
+    if !expand {
+        return galaxy_map;
     }
     let mut r_index = 0;
     let const_column_size = galaxy_map[0].len();
@@ -71,7 +74,6 @@ pub fn parse_input_expanded(mut lines: Vec<String>) -> Vec<Vec<u8>> {
         }
         c_index += 1;
     }
-
     galaxy_map
 }
 
@@ -103,4 +105,101 @@ impl Display for Point {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.write_str(&format!("[{}, {}]", self.row, self.col))
     }
+}
+
+
+const EXPANSION_SIZE: usize = 1_000_000;
+pub fn galaxy2(lines: Vec<String>) -> usize {
+    let galaxy = parse_input(lines, false);
+    let mut galaxies = vec![];
+    for (r, row) in galaxy.iter().enumerate() {
+        for (c, col) in row.iter().enumerate() {
+            if *col == b'#' {
+                galaxies.push(Point::new(r, c));
+            }
+        }
+    }
+    let void_rows = find_void_rows(&galaxy);
+    let void_columns = find_void_columns(&galaxy);
+    let mut distance_sum = 0;
+    for i in 0..galaxies.len() - 1 {
+        for j in i + 1..galaxies.len() {
+            let mut point1 = &galaxies[i];
+            let mut point2 = &galaxies[j];
+            if point1.row > point2.row {
+                let temp = point1;
+                point1 = point2;
+                point2 = temp;
+            }
+            let mut row_voids_crossed = 0;
+            for void_row in &void_rows {
+                if point1.row < *void_row && *void_row < point2.row {
+                    row_voids_crossed += 1;
+                }
+            }
+            if point1.col > point2.col {
+                let temp = point1;
+                point1 = point2;
+                point2 = temp;
+            }
+            let mut col_voids_crossed = 0;
+            for void_col in &void_columns {
+                if point1.col < *void_col && *void_col < point2.col {
+                    col_voids_crossed += 1;
+                }
+            }
+
+            let row_displacement = ((EXPANSION_SIZE - 1) * row_voids_crossed) as isize;
+            let col_displacement = ((EXPANSION_SIZE - 1) * col_voids_crossed) as isize;
+            let (mut row1, mut col1) = (point1.row as isize, point1.col as isize);
+            if point1.row < point2.row {
+                row1 -= row_displacement;
+            } else {
+                row1 += row_displacement;
+            }
+            if point1.col < point2.col {
+                col1 -= col_displacement;
+            } else {
+                col1 += col_displacement;
+            }
+            let space_distance = isize_distance(row1, col1, point2.row as isize, point2.col as isize);
+            distance_sum += space_distance;
+        }
+    }
+    distance_sum
+}
+
+pub fn find_void_rows(galaxy_map: &Vec<Vec<u8>>) -> Vec<usize> {
+    let mut rows = vec![];
+    for (i, row) in galaxy_map.iter().enumerate() {
+        if !row.contains(&b'#') {
+            rows.push(i);
+        }
+    }
+    rows
+}
+pub fn find_void_columns(galaxy_map: &Vec<Vec<u8>>) -> Vec<usize> {
+    let mut columns = vec![];
+    let const_col_length = galaxy_map[0].len();
+    let const_row_length = galaxy_map.len();
+    for col_index in 0..const_col_length {
+        let mut has_galaxy = false;
+        for row_index in 0..const_row_length {
+            let value = &galaxy_map[row_index][col_index];
+            if *value == b'#' {
+                has_galaxy = true;
+                break;
+            }
+        }
+        if !has_galaxy {
+            columns.push(col_index);
+        }
+    }
+    columns
+}
+
+pub fn isize_distance(row1: isize, col1: isize, row2: isize, col2: isize) -> usize {
+    let row_diff = row1 - row2;
+    let col_diff = col1 - col2;
+    (row_diff.abs() + col_diff.abs()) as usize
 }
--- src/tests/test_day11.rs
+++ src/tests/test_day11.rs
@@ -16,9 +16,31 @@ fn test_full_part1() {
 }
 
 #[test]
+fn test_demo_part2() {
+    /*let lines = days::read_lines(11, InputType::DemoPart1);
+    let distance_sum = day11::galaxy2(lines);
+    // only for EXPANSION_SIZE of 10
+    assert_eq!(distance_sum, 1030)*/
+}
+
+#[test]
+fn test_full_part2() {
+    let lines = days::read_lines(11, InputType::Full);
+    let distance_sum = day11::galaxy2(lines);
+    assert_eq!(distance_sum, 644248339497)
+}
+
+#[test]
+fn test_custom_part2() {
+    let lines = days::read_lines(11, InputType::Custom("custom/simple_galaxy.txt".into()));
+    let distance_sum = day11::galaxy2(lines);
+    assert_eq!(distance_sum, 1000001)
+}
+
+#[test]
 fn galaxy_transformation() {
     let demo_lines = days::read_lines(11, InputType::DemoPart1);
-    let galaxy = day11::parse_input_expanded(demo_lines);
+    let galaxy = day11::parse_input(demo_lines, true);
     let correct_transform_input = InputType::Custom("custom/expanded_galaxy.txt".into());
     let expanded_lines = days::read_lines(11, correct_transform_input);
     assert_eq!(galaxy.len(), expanded_lines.len());
@@ -43,4 +65,20 @@ fn galaxy_distance() {
     let galaxy6 = Point::new(7, 12);
     assert_eq!(galaxy3.manhattan_distance(&galaxy6), 17);
     assert_eq!(galaxy6.manhattan_distance(&galaxy3), 17)
+}
+
+#[test]
+fn void_test() {
+    let demo_lines = days::read_lines(11, InputType::DemoPart1);
+    let galaxy = day11::parse_input(demo_lines, false);
+    let vec_rows = day11::find_void_rows(&galaxy);
+    let vec_cols = day11::find_void_columns(&galaxy);
+
+    assert_eq!(vec_rows.len(), 2);
+    assert_eq!(vec_rows[0], 3);
+    assert_eq!(vec_rows[1], 7);
+    assert_eq!(vec_cols.len(), 3);
+    assert_eq!(vec_cols[0], 2);
+    assert_eq!(vec_cols[1], 5);
+    assert_eq!(vec_cols[2], 8);
 }
