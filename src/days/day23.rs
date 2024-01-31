af1d81615b9cc44b528b6a0210ae437a0d01e112
FriskIsGit <friskisgit@users.noreply.github.com>
2024-01-31 12:39:48 +0100

    ready dijkstra

--- src/days/day23.rs
+++ src/days/day23.rs
@@ -4,10 +4,15 @@ use std::fmt::{Display, Formatter};
 pub fn long_walk1(lines: Vec<String>) -> usize {
     let map = read_map(lines);
     print_map(&map);
+    let walkable = count_byte(&map, b'.');
     let dijkstra = Dijkstra::new_detect(map);
-    println!("start: {}", dijkstra.start);
-    println!("end: {}", dijkstra.end);
-    print_distance_map(&dijkstra.distances);
+    println!("start: {} | end: {}", dijkstra.start, dijkstra.end);
+    println!("walkable: {walkable}");
+    let mut current = dijkstra.start.clone();
+    // alternatively run until all nodes are explored?
+    while !current.eq(&dijkstra.end) {
+        current = dijkstra.end.clone();
+    }
     0
 }
 
@@ -41,6 +46,7 @@ pub fn print_distance_map(distances: &Vec<Vec<u32>>) {
         }
         column_width[col_index] = max_width_in_row;
     }
+    let mut format = String::with_capacity(distances.len() * distances[0].len());
     for row in distances {
         let mut c_index = 0;
         let mut line = String::new();
@@ -53,8 +59,22 @@ pub fn print_distance_map(distances: &Vec<Vec<u32>>) {
             }
             c_index += 1;
         }
-        println!("{line}");
+        format.push_str(&line);
+        format.push('\n');
     }
+    println!("{}", format);
+}
+
+pub fn count_byte(map: &Vec<Vec<u8>>, byte: u8) -> usize {
+    let mut count = 0;
+    for row in map {
+        for b in row {
+            if *b == byte {
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 trait Length {
@@ -72,22 +92,22 @@ pub struct Dijkstra {
     end: Point,
     visited: HashSet<u16>, // visited hashes
     map: Vec<Vec<u8>>, // map with bytes: b'#', b'>', b'v', b'.'
-    distances: Vec<Vec<u32>>,
+    nodes: Vec<Vec<Node>>
 }
 
 impl Dijkstra {
     pub fn new(map: Vec<Vec<u8>>, start: Point, end: Point) -> Self {
-        let mut distances = vec![vec![15u32; map[0].len()]; map.len()];
-        distances[start.row][start.col] = 0;
-        Self { map, start, end, visited: HashSet::with_capacity(32), distances }
+        let mut nodes = vec![vec![Node::new(); map[0].len()]; map.len()];
+        nodes[start.row][start.col].best_distance = 0;
+        Self { map, start, end, visited: HashSet::with_capacity(32), nodes }
     }
 
     pub fn new_detect(map: Vec<Vec<u8>>) -> Self {
         let start = Self::find_start(&map);
         let end = Self::find_end(&map);
-        let mut distances = vec![vec![15u32; map[0].len()]; map.len()];
-        distances[start.row][start.col] = 0;
-        Self { map, start, end, visited: HashSet::with_capacity(32), distances }
+        let mut nodes = vec![vec![Node::new(); map[0].len()]; map.len()];
+        nodes[start.row][start.col].best_distance = 0;
+        Self { map, start, end, visited: HashSet::with_capacity(32), nodes }
     }
 
     pub fn is_visited(&self, row: u16, col: u16) -> bool {
@@ -138,6 +158,29 @@ impl Dijkstra {
     }
 }
 
+type Hash = u16;
+#[derive(Clone)]
+pub struct Node {
+    origin: Option<Hash>,
+    best_distance: u32
+}
+
+const MAX_DISTANCE: u32 = 9999;
+impl Node {
+    pub fn new() -> Self {
+        Self { origin: None, best_distance: MAX_DISTANCE}
+    }
+
+    pub fn get_origin(&self) -> Option<Hash> {
+        self.origin
+    }
+
+    pub fn set_origin(&mut self, hash: Hash) {
+        self.origin = Some(hash);
+    }
+}
+
+#[derive(Clone)]
 pub struct Point {
     row: usize,
     col: usize,
@@ -146,6 +189,9 @@ pub struct Point {
 impl Point {
     pub fn new(row: usize, col: usize) -> Self {
         Self { row, col }
+    }
+    pub fn eq(&self, point: &Point) -> bool {
+        self.row == point.row && self.col == point.col
     }
 }
 
